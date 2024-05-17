49e353a503baa1e1871a0572a959d3289185bf19
Adam Duskett <adam.duskett@amarulasolutions.com>
2024-05-17 14:56:22 -0600

    A lot of cleanup and safety checks

--- install-rEFInd-GUI.sh
+++ install-rEFInd-GUI.sh
@@ -1,104 +1,136 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
 # A simple script to install the rEFInd customization GUI
+BUILD_DIR=$(mktemp -d)
+GIT_DIR="${BUILD_DIR}"/rEFInd_GUI
+BAZZITE="/etc/bazzite/image_name"
+FEDORA_BASE=$(command -v dnf || true)
+USER="$(whoami)"
 
-echo -e "Installing rEFInd Customization GUI...\n"
-cd $HOME
-sudo rm -rf ./rEFInd_GUI
-git clone https://github.com/jlobue10/rEFInd_GUI
-cd rEFInd_GUI
-CURRENT_WD=$(pwd)
-
-mkdir -p $HOME/.local/rEFInd_GUI
-cp -rf $CURRENT_WD/GUI/ $HOME/.local/rEFInd_GUI
-cp -rf $CURRENT_WD/icons/ $HOME/.local/rEFInd_GUI
-cp -rf $CURRENT_WD/backgrounds/ $HOME/.local/rEFInd_GUI
-cp -f $CURRENT_WD/{refind_install_package_mgr.sh,refind_install_Sourceforge.sh} $HOME/.local/rEFInd_GUI
-cp -f $CURRENT_WD/refind-GUI.conf $HOME/.local/rEFInd_GUI/GUI/refind.conf
-
-chmod +x $HOME/.local/rEFInd_GUI/{refind_install_package_mgr.sh,refind_install_Sourceforge.sh}
-
-which dnf
-FEDORA_BASE=$?
-
-cat /etc/nobara-release
-NOBARA=$?
-
-cat /etc/bazzite/image_name
-BAZZITE=$?
-
-if [ $FEDORA_BASE == 0 ] && [ $BAZZITE != 0 ]; then
-	echo -e '\nFedora based installation starting.\n'
- 	if [ $NOBARA == 0 ]; then
-		sudo dnf install -y cmake hwinfo gcc-c++ qt6-qtbase-devel qt6-qttools-devel qt5-qtbase-devel qt5-qttools-devel xterm
-  	else
-   		sudo dnf install -y cmake hwinfo gcc-c++ qt6-qtbase-devel qt6-qttools-devel qt5-qtbase-devel qt5-qttools-devel xterm
-     	fi
-	mkdir -p $HOME/rpmbuild/{SPECS,SOURCES}
-	cp rEFInd_GUI.spec $HOME/rpmbuild/SPECS
-	rpmbuild -bb $HOME/rpmbuild/SPECS/rEFInd_GUI.spec
- 	sudo dnf list --installed | grep rEFInd_GUI
-  	REFIND_GUI_STATUS=$?
-   	if [ $REFIND_GUI_STATUS == 0 ]; then
-    		sudo dnf remove -y rEFInd_GUI
-	fi
-	sudo dnf install -y $HOME/rpmbuild/RPMS/x86_64/rEFInd_GUI*.rpm
+if [[ "${EUID}" == 0 ]]; then
+  echo "Do not run this script as root!"
+  exit 1
 fi
 
-if [ $BAZZITE == 0 ]; then
-	echo -e '\nBazzite based installation starting.\n'
- 	rpm-ostree status | grep xterm
-  	XTERM_STATUS=$?
-   	if [ $XTERM_STATUS != 0]; then
-		sudo rpm-ostree install xterm
-    	fi
- 	cd $HOME/Downloads
-  	rm -f rEFInd_GUI*.rpm
-  	wget $(curl -s https://api.github.com/repos/jlobue10/rEFInd_GUI/releases/latest | grep "browser_download_url.*\.rpm" | grep -v "\.src\.rpm" | cut -d : -f 2,3 | tr -d \"\ )
-   	sudo rpm-ostree install ./rEFInd_GUI*.rpm
+
+message() {
+  printf "\n##### %s #####\n" "${1}"
+}
+
+
+clone() {
+  message "Cloning https://github.com/jlobue10/rEFInd_GUI to ${GIT_DIR}"
+  if [[ ! -d "${GIT_DIR}" ]]; then
+    cd "${BUILD_DIR}"
+    git clone https://github.com/jlobue10/rEFInd_GUI
+  fi
+}
+
+
+copy_local_files() {
+  local_dir="${HOME}"/.local/rEFInd_GUI
+  message "Copying rEFInd_GUI files to ${local_dir}"
+  rm -rf "${local_dir}" && mkdir -p "${local_dir}"
+  # Directories
+  cp -dprf "${GIT_DIR}"/{GUI,icons,backgrounds} "${local_dir}"/
+  # Hard links
+  cp -f "${GIT_DIR}"/{refind_install_package_mgr.sh,refind_install_Sourceforge.sh} "${local_dir}"/
+  cp -f "${GIT_DIR}"/refind-GUI.conf "${local_dir}"/GUI/refind.conf
+  chmod +x "${local_dir}"/{refind_install_package_mgr.sh,refind_install_Sourceforge.sh}
+}
+
+
+install_fedora_dependencies() {
+  i=0
+  rpms=("${@}")
+  declare -A not_installed
+  for rpm in "${rpms[@]}"; do
+    check=$(dnf list --installed |grep "${rpm}" || true)
+    if [[ -z "${check}" ]]; then
+      not_installed[${i}]="${rpm}"
+      i=$(( "${i}" + 1 ))
+    fi
+  done
+  if [[ "${i}" -gt 0 ]]; then
+    echo "Installing: ${not_installed[*]}"
+    sudo dnf install -y "${not_installed[*]}"
+  fi
+  check=$(sudo dnf list --installed | grep "rEFInd_GUI" || true)
+  if [[ -z "${check}" ]]; then
+    sudo dnf remove -y rEFInd_GUI
+  fi
+}
+
+
+rpm_build() {
+  rpmbuild_dir="${HOME}"/rpmbuild/
+  rm -rf "${rpmbuild_dir}"
+  mkdir -p "${rpmbuild_dir}"/{SPECS,SOURCES}
+  cp "${GIT_DIR}"/rEFInd_GUI.spec "${HOME}"/rpmbuild/SPECS
+  cd "${rpmbuild_dir}"
+  rpmbuild -bb "${HOME}"/rpmbuild/SPECS/rEFInd_GUI.spec
+  sudo dnf install -y "${HOME}"/rpmbuild/RPMS/x86_64/rEFInd_GUI*.rpm
+}
+
+
+build_fedora() {
+  message 'Fedora based installation starting.'
+  rpms=( "cmake" "hwinfo" "gcc-c++" "git" "qt5-qtbase-devel" "qt5-qttools-devel" "rpm-build" "wget" )
+  install_fedora_dependencies "${rpms[@]}"
+  rpm_build
+}
+
+clone
+copy_local_files
+
+if [[ -n "${FEDORA_BASE}" ]] && [[ -e "${BAZZITE}" ]]; then
+  build_fedora
+fi
+
+if [[ -e "${BAZZITE}" ]]; then
+  message 'Bazzite based installation starting.'
+  check=$(rpm-ostree status | grep xterm || true)
+  if [[ -z "${check}" ]]; then
+    sudo rpm-ostree install xterm
+  fi
+  cd "${HOME}"/Downloads
+  rm -f rEFInd_GUI*.rpm
+  latest_rpm="$(curl -s https://api.github.com/repos/jlobue10/rEFInd_GUI/releases/latest | grep "browser_download_url.*\.rpm" | grep -v "\.src\.rpm" | cut -d : -f 2,3 | tr -d \"\ || true)"
+  if [[ -n "${latest_rpm}" ]]; then
+    wget "${latest_rpm}"
+    sudo rpm-ostree install ./"${latest_rpm}"
+  else
+    message "Unable to get the latest rEFINd_GUI!"
+    exit 1
+  fi
 fi   
 
-which apt 2>/dev/null
-UBUNTU_BASE=$?
-
-if [ $UBUNTU_BASE == 0 ]; then
-	echo -e '\nUbuntu based installation starting.\n'
-	#sudo apt-get update && sudo apt-get install build deps
-fi
-
-which pacman 2>/dev/null
-ARCH_BASE=$?
-
-# if [ $NOBARA == 0 ]; then
-#	#fix packaging after compile (if necessary)
-#	sudo dnf install gstreamer1-plugins-good-qt6 --allowerasing
-# fi
-
-sed -i "s@USER@$USER@g" $CURRENT_WD/install_config_from_GUI
-sed -i "s@HOME@/home/$USER@g" $CURRENT_WD/rEFInd_GUI.desktop
-sed -i "s@HOME@/home/$USER@g" $CURRENT_WD/install_config_from_GUI.sh
-sed -i "s@USER@$USER@g" $CURRENT_WD/rEFInd_bg_randomizer.sh
+sed -i "s@USER@${USER}@g" "${GIT_DIR}"/install_config_from_GUI
+sed -i "s@HOME@/home/${USER}@g" "${GIT_DIR}"/rEFInd_GUI.desktop
+sed -i "s@HOME@/home/${USER}@g" "${GIT_DIR}"/install_config_from_GUI.sh
+sed -i "s@USER@${USER}@g" "${GIT_DIR}"/rEFInd_bg_randomizer.sh
 
 sudo mkdir -p /etc/rEFInd
-sudo cp -f $CURRENT_WD/install_config_from_GUI /etc/sudoers.d/install_config_from_GUI
-sudo cp -f $CURRENT_WD/install_config_from_GUI.sh /etc/rEFInd/install_config_from_GUI.sh
-sudo cp -f $CURRENT_WD/rEFInd_bg_randomizer.sh /etc/rEFInd/rEFInd_bg_randomizer.sh
-sudo cp -f $CURRENT_WD/GUI/UEFI_icon.png /etc/rEFInd/UEFI_icon.png
+sudo cp -f "${GIT_DIR}"/install_config_from_GUI /etc/sudoers.d/install_config_from_GUI
+sudo cp -f "${GIT_DIR}"/install_config_from_GUI.sh /etc/rEFInd/install_config_from_GUI.sh
+sudo cp -f "${GIT_DIR}"/rEFInd_bg_randomizer.sh /etc/rEFInd/rEFInd_bg_randomizer.sh
+sudo cp -f "${GIT_DIR}"/GUI/UEFI_icon.png /etc/rEFInd/UEFI_icon.png
 
-if [ $BAZZITE == 0 ]; then
-	sudo cp -f $CURRENT_WD/rEFInd_GUI.desktop /etc/rEFInd/rEFInd_GUI.desktop
- 	sudo chmod +x /etc/rEFInd/rEFInd_GUI.desktop
- 	cp $CURRENT_WD/rEFInd_GUI.desktop $HOME/Desktop/refind_GUI.desktop
-  	cp $CURRENT_WD/.Xresources $HOME/.Xresources
-   	xrdb $HOME/.Xresources
+if [[ -e "${BAZZITE}" ]]; then
+  sudo cp -f "${GIT_DIR}"/rEFInd_GUI.desktop /etc/rEFInd/rEFInd_GUI.desktop
+  sudo chmod +x /etc/rEFInd/rEFInd_GUI.desktop
+  cp "${GIT_DIR}"/rEFInd_GUI.desktop "${HOME}"/Desktop/refind_GUI.desktop
+  cp "${GIT_DIR}"/.Xresources "${HOME}"/.Xresources
+  xrdb "${HOME}"/.Xresources
 else
-	sudo cp -f $CURRENT_WD/rEFInd_GUI.desktop /usr/share/applications/rEFInd_GUI.desktop
-  	cp /usr/share/applications/rEFInd_GUI.desktop $HOME/Desktop/refind_GUI.desktop
+  sudo cp -f "${GIT_DIR}"/rEFInd_GUI.desktop /usr/share/applications/rEFInd_GUI.desktop
+  cp /usr/share/applications/rEFInd_GUI.desktop "${HOME}"/Desktop/refind_GUI.desktop
 fi
 
-chmod +x $HOME/Desktop/refind_GUI.desktop
-
+chmod +x "${HOME}"/Desktop/refind_GUI.desktop
 sudo chmod +x /etc/rEFInd/{install_config_from_GUI.sh,rEFInd_bg_randomizer.sh}
 
-if [ $BAZZITE == 0 ]; then
-	systemctl reboot
+if [[ -e "${BAZZITE}" ]]; then
+   systemctl reboot
 fi
--- install_config_from_GUI.sh
+++ install_config_from_GUI.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
-cp HOME/.local/rEFInd_GUI/GUI/{refind.conf,background.png,os_icon1.png,os_icon2.png,os_icon3.png,os_icon4.png} /boot/efi/EFI/refind/
+#!/usr/bin/env bash
+set -e
+REFIND_GUI_DIR="${HOME}/.local/rEFInd_GUI/GUI"
+REFIND_BOOT_DIR="/boot/efi/EFI/refind"
+
+if [[ ! -d "${REFIND_GUI_DIR}" ]]; then
+  echo "${REFIND_GUI_DIR}: No such directory!"
+  exit 1
+fi
+
+if [[ ! -d "${REFIND_BOOT_DIR}" ]]; then
+  echo "${REFIND_BOOT_DIR} No such directory!"
+  exit 1
+fi
+
+cp "${HOME}"/.local/rEFInd_GUI/GUI/{refind.conf,background.png,os_icon1.png,os_icon2.png,os_icon3.png,os_icon4.png} /boot/efi/EFI/refind/
